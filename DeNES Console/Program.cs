149765a11443ba64867adb79a263150e501ac948
Dettomi <detto1998@gmail.com>
2024-11-09 15:56:01 +0100

    Adding Console, WPF projects

---
+++ DeNES Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using DeNES_ClassLibrary;
+
+namespace DeNES_Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DeNES emulator = new DeNES();
+            emulator.Run("Mario Bros. (World).nes");
+        }
+    }
+}
